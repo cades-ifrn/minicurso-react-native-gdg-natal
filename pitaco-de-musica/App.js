e9ede7e52cc838352f39553fd13475e4bafb101b
Felipe Pontes <felipemfpontes@gmail.com>
2018-01-22 11:41:30 -0300

    Add step-1

--- pitaco-de-musica/App.js
+++ pitaco-de-musica/App.js
@@ -1,23 +1,26 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { Platform, StatusBar } from 'react-native'
+import { StackNavigator } from 'react-navigation'
+
+import HomeScreen from './screens/HomeScreen'
+
+const RootNavigator = StackNavigator({
+  Home: {
+    screen: HomeScreen,
+    navigationOptions: {
+      header: null
+    }
+  }
+}, {
+  cardStyle: {
+    paddingTop: Platform.OS === 'ios' ? 0 : StatusBar.currentHeight
+  }
+})
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
+      <RootNavigator />
+    )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ pitaco-de-musica/screens/HomeScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+class HomeScreen extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>Olá da HomeScreen</Text>
+      </View>
+    )
+  }
+}
+
+export default HomeScreen
