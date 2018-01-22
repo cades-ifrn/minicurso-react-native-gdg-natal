b2e4f1b72df39184505785e4e02a4dd30f96aa7a
Felipe Pontes <felipemfpontes@gmail.com>
2018-01-22 14:19:45 -0300

    Add step-4

---
+++ pitaco-de-musica/constants.js
@@ -0,0 +1,1 @@
+export const API_KEY = '__API_KEY__'
--- pitaco-de-musica/screens/SearchScreen.js
+++ pitaco-de-musica/screens/SearchScreen.js
@@ -1,14 +1,96 @@
 import React from 'react'
-import { StyleSheet, View, FlatList, Text } from 'react-native'
+import { StyleSheet, View, FlatList, Text, TextInput, Button, ActivityIndicator } from 'react-native'
+import { API_KEY } from '../constants'
+import axios from 'axios'
 
 class SearchScreen extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      track: '',
+      artist: '',
+      results: [],
+      loading: false
+    }
+  }
+
+  _search() {
+    if (this.state.artist === '' || this.state.track === '' || this.loading) return
+
+    this.setState({loading: true})
+    axios.get('http://ws.audioscrobbler.com/2.0/', {
+      params: {
+        method: 'track.getsimilar',
+        format: 'json',
+        artist: this.state.artist,
+        track: this.state.track,
+        api_key: API_KEY
+      }
+    }).then(response => {
+      const {data} = response
+
+      console.log(data)
+
+      this.setState({
+        results: data.similartracks.track,
+        loading: false
+      })
+    }).catch(() => {
+      this.setState({
+        results: [],
+        loading: false
+      })
+    })
+  }
+
+  _keyExtractor(item, index) {
+    return index
+  }
+
+  _renderItem({item}) {
+    return (
+      <Text>{item.name} - {item.artist.name}</Text>
+    )
+  }
+
   render() {
     return (
       <View>
-        <Text>Olá da SearchScreen</Text>
+        <View style={styles.form}>
+          <Text style={styles.label}>Artista</Text>
+          <TextInput 
+            onBlur={() => this._search()}
+            onChangeText={(text) => this.setState({artist: text})}
+            style={styles.searchInput} />
+          <Text style={styles.label}>Música</Text>
+          <TextInput 
+            onBlur={() => this._search()}
+            onChangeText={(text) => this.setState({track: text})}
+            style={styles.searchInput} />
+        </View>
+        { this.state.loading 
+          ? <ActivityIndicator />
+          : <FlatList 
+              data={this.state.results}
+              renderItem={this._renderItem}
+              keyExtractor={this._keyExtractor}
+            />
+        }
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  form: {
+    padding: 10
+  },
+  label: {
+    fontWeight: 'bold'
+  },
+  searchInput: {
+    height: 40,
+  }
+})
 
 export default SearchScreen
